b3f7c07005713a320c2fd0ba208e57c62d7a95aa
Youthink <hufy3651@foxmail.com>
2020-11-16 20:51:52 +0800

    perfect

--- netease-rich/index.js
+++ netease-rich/index.js
@@ -1,21 +1,34 @@
 const fs = require('fs');
+const path =require('path');
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
-const download = require('./download');
 const koaJson2xlsx = require('koa-json2xlsx');
+const render = require('koa-ejs');
+const download = require('./download');
+const login = require('./login');
 
 const app = new Koa();
 
+render(app, {
+  root: path.join(__dirname),
+  layout: false
+});
+
 const main = async (ctx) => {
-  if (ctx.request.path === '/result' && ctx.method === 'POST') {
+  if (ctx.request.path === '/login' && ctx.method === 'POST') {
     const postData = ctx.request.body;
     const { email, password } = postData || {};
-    const data = await download(email, password);
-    ctx.xlsx(`netease-rich-${Date.now()}.xlsx`, data);
+    const { token, cookiesStr, error } = await login(email, password);
+    if (error) {
+      ctx.render('index', {error: '登录失败，请检查邮箱和密码是否正确~'});
+    } else {
+      const data = await download(token, cookiesStr);
+      console.log('debug: success_export_data');
+      ctx.xlsx(`netease-rich-${Date.now()}.xlsx`, data);
+    }
   } else {
-    ctx.response.type = 'html';
-    ctx.response.body = fs.createReadStream('./index.html');
-  };
+    ctx.render('index', { error: '' });
+  }
 }
 
 
