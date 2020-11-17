ac79b6d85a55c1ba7529fa28ee98fa4b3a34761e
Youthink <hufy3651@foxmail.com>
2020-11-17 19:52:49 +0800

    optimize puppeteer

--- netease-rich/index.js
+++ netease-rich/index.js
@@ -15,16 +15,16 @@ render(app, {
   layout: false
 });
 
-const browser = await puppeteer.launch({
-  headless: true,
+const brower = puppeteer.launch({
+  headless: false,
   args: [
-    '–disable-gpu', // GPU硬件加速
-    '–disable-dev-shm-usage', // 创建临时文件共享内存
-    '–disable-setuid-sandbox', // uid沙盒
-    '–no-first-run', // 没有设置首页。在启动的时候，就会打开一个空白页面。
-    '–no-sandbox', // 沙盒模式
-    '–no-zygote',
-    '–single-process' // 单进程运行
+    '--disable-gpu', // GPU硬件加速
+    '--disable-dev-shm-usage', // 创建临时文件共享内存
+    '--disable-setuid-sandbox', // uid沙盒
+    '--no-first-run', // 没有设置首页。在启动的时候，就会打开一个空白页面。
+    '--no-sandbox', // 沙盒模式
+    '--no-zygote',
+    '--single-process' // 单进程运行
   ]
 });
 
@@ -32,12 +32,13 @@ const main = async (ctx) => {
   if (ctx.request.path === '/login' && ctx.method === 'POST') {
     const postData = ctx.request.body;
     const { email, password } = postData || {};
+    const browser = await brower;
     const { token, cookiesStr, error } = await login(browser, email, password);
     if (error) {
-      ctx.render('index', {error: '登录失败，请检查邮箱和密码是否正确~'});
+      ctx.render('index', { error });
     } else {
       const data = await download(token, cookiesStr);
-      console.log('debug: success_export_data');
+      console.log('debug:success_export_data');
       ctx.xlsx(`netease-rich-${Date.now()}.xlsx`, data);
     }
   } else {
